256b374ca52d8d76f82adbd7821229df66c7f05a
Patrick Naughton <patricknaughton01@gmail.com>
2022-05-12 22:41:16 -0500

    Add point serialization
    Need to call `serialize` to serialize a point. This will malloc
    some memory and store the serialization there. The memory gets
    freed automatically by the point when it gets destroyed.

--- include/point.h
+++ include/point.h
@@ -2,15 +2,24 @@
 #define POINT_H
 
 class Point{
+private:
+    bool has_serialized;
+    char *m_s_buf;
 public:
     float x, y, z;
     unsigned short r, g, b;
+    size_t s_buf_size;
     Point(float _x, float _y, float _z,
         unsigned short _r, unsigned short _g, unsigned short _b):
-        x(_x), y(_y), z(_z), r(_r), g(_g), b(_b){};
+        x(_x), y(_y), z(_z), r(_r), g(_g), b(_b),
+        s_buf_size(3 * sizeof(float) + 3 * sizeof(unsigned short)),
+        has_serialized(false){};
     Point(const Point &p);
+    ~Point();
     float d_dist(const Point &p) const;
     float c_dist(const Point &p) const;
+    const char * const serialize();
+    const char * const s_buf() const;
 };
 
 #endif // POINT_H
--- src/point.cpp
+++ src/point.cpp
@@ -4,6 +4,16 @@
 
 Point::Point(const Point &p){
     x = p.x; y = p.y; z = p.z; r = p.r; g = p.g; b = p.b;
+    if(p.has_serialized){
+        serialize();
+    }
+}
+
+
+Point::~Point(){
+    if(has_serialized){
+        free(m_s_buf);
+    }
 }
 
 
@@ -18,4 +28,27 @@ float Point::c_dist(const Point &p) const{
     return std::sqrt(std::pow(r - p.r, 2)
         + std::pow(g - p.g, 2)
         + std::pow(b - p.b, 2));
+}
+
+
+const char * const Point::serialize(){
+    if(!has_serialized){
+        m_s_buf = (char*)malloc(s_buf_size);
+    }
+    float *d_start = (float*)m_s_buf;
+    *d_start = x;
+    *(d_start + 1) = y;
+    *(d_start + 2) = z;
+    unsigned int f_size = sizeof(float), i_size = sizeof(unsigned int);
+    unsigned int *c_start = ((unsigned int*)m_s_buf) + 3 * f_size / i_size;
+    *c_start = r;
+    *(c_start + 1) = g;
+    *(c_start + 2) = b;
+    has_serialized = true;
+    return m_s_buf;
+}
+
+
+const char * const Point::s_buf() const{
+    return m_s_buf;
 }
