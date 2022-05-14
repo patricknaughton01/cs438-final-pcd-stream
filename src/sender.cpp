448704d5a15be1ae65000a09bc01953a5ec0af8f
Patrick Naughton <patricknaughton01@gmail.com>
2022-05-13 22:28:54 -0500

    Sender working?

--- src/sender.cpp
+++ src/sender.cpp
@@ -358,11 +358,11 @@ int main(int argc, char** argv) {
     std::mutex points_lock;
     std::deque<Point> points;
     bool finished_flag = false;
-    // std::thread transfer_thread(reliablyTransfer, argv[1], udpPort,
-    //     std::ref(points), std::ref(points_lock), std::ref(finished_flag));
+    std::thread transfer_thread(reliablyTransfer, argv[1], udpPort,
+        std::ref(points), std::ref(points_lock), std::ref(finished_flag));
     load_points(argv[3], points, points_lock);
     finished_flag = true;
-    // transfer_thread.join();
+    transfer_thread.join();
 
     return (EXIT_SUCCESS);
 }
